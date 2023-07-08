315ff51bb058ebd371dc6a12330ae0e01065641a
Yokota Yoshinori <yokota@yynet.org>
2023-07-08 14:00:44 +0900

    Add Seat/Reserve to admin.site.register

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Seat)
+admin.site.register(models.Reserve)
